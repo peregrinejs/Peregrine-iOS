dc39dba86abcd40fbb928d02a13fab9800ddf81d
Horus Lugo <horusgoul@gmail.com>
2023-09-23 18:24:45 +0200

    feat: support configuring webView.scrollView.bounce

---
+++ Sources/Peregrine/WebViewOptions.swift
@@ -0,0 +1,19 @@
+
+
+public struct WebViewScrollViewOptions {
+    public let bounces: Bool
+    
+    public init(
+        bounces: Bool = false
+    ) {
+        self.bounces = bounces
+    }
+}
+
+public struct WebViewOptions {
+    public let scrollView: WebViewScrollViewOptions
+    
+    public init(scrollView: WebViewScrollViewOptions = WebViewScrollViewOptions()) {
+        self.scrollView = scrollView
+    }
+}
